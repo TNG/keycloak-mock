00b211ca1dd8a66c131c885f6e637b52435dc6f2
Kai Helbig <kai.helbig@tngtech.com>
2021-01-22 09:45:36 +0100

    fix sonar warning about log injection
    Signed-off-by: Kai Helbig <kai.helbig@tngtech.com>

---
+++ standalone/src/main/java/com/tngtech/keycloakmock/standalone/helper/UserInputSanitizer.java
@@ -0,0 +1,27 @@
+package com.tngtech.keycloakmock.standalone.helper;
+
+import java.util.Objects;
+import javax.annotation.Nullable;
+
+public class UserInputSanitizer {
+
+  @Nullable private final String input;
+  @Nullable private String sanitized;
+
+  public UserInputSanitizer(@Nullable String input) {
+    this.input = input;
+  }
+
+  @Nullable
+  public String getSanitizedInput() {
+    if (input != null && sanitized == null) {
+      sanitized = input.replaceAll("[\n\r\t|]", "_");
+    }
+    return sanitized;
+  }
+
+  @Override
+  public String toString() {
+    return Objects.toString(getSanitizedInput());
+  }
+}
