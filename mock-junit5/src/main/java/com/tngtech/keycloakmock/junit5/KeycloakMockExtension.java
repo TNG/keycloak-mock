0255e5ed042237c3d0c230d782546f0730bf9092
André Conrado <andre.conrado.0@gmail.com>
2021-02-05 17:53:47 +0000

    Added TokenRoute to mock-junit5 Signed-off-by: André Conrado <andre.conrado@gls-itservices.com>

--- mock-junit5/src/main/java/com/tngtech/keycloakmock/junit5/KeycloakMockExtension.java
+++ mock-junit5/src/main/java/com/tngtech/keycloakmock/junit5/KeycloakMockExtension.java
@@ -1,13 +1,14 @@
 package com.tngtech.keycloakmock.junit5;
 
-import com.tngtech.keycloakmock.api.KeycloakMock;
-import com.tngtech.keycloakmock.api.ServerConfig;
-import com.tngtech.keycloakmock.api.TokenConfig;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import com.tngtech.keycloakmock.api.KeycloakMock;
+import com.tngtech.keycloakmock.api.ServerConfig;
+import com.tngtech.keycloakmock.api.TokenConfig;
+import com.tngtech.keycloakmock.impl.handler.TokenRoute;
 
 /**
  * A JUnit5 extension to be used to automatically start and stop the keycloak mock.
@@ -51,6 +52,26 @@ public class KeycloakMockExtension implements BeforeAllCallback, AfterAllCallbac
    */
   public KeycloakMockExtension(@Nonnull final ServerConfig serverConfig) {
     mock = new KeycloakMock(serverConfig);
+  }
+
+  /**
+   * Get {@link TokenRoute} handler and control endpoit responses.
+   *
+   * <p>Example use:
+   *
+   * <pre><code>
+   * {@literal //} return error 404
+   * getTokenRoute().withErrorResponse(404, "{\"error\": \"Error detail message\"}")
+   *
+   * {@literal //} return 200
+   * getTokenRoute().withOkResponse(accessTokenConfig, idTokenConfig, refreshTokenConfig, 60 * 60);
+   * </code></pre>
+   *
+   * @return token route handler
+   * @see TokenRoute
+   */
+  public TokenRoute getTokenRoute() {
+    return mock.getTokenRoute();
   }
 
   /**
