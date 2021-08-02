62872a30951ccde1550af89f3a0f74225c7a2157
Kai Helbig <kai.helbig@tngtech.com>
2021-08-02 18:30:11 +0200

    support confidential clients in password grant flow
    Signed-off-by: Kai Helbig <kai.helbig@tngtech.com>
    
    #87

---
+++ mock/src/main/java/com/tngtech/keycloakmock/impl/handler/OptionalBasicAuthHandler.java
@@ -0,0 +1,38 @@
+package com.tngtech.keycloakmock.impl.handler;
+
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.ext.auth.User;
+import io.vertx.ext.auth.authentication.Credentials;
+import io.vertx.ext.auth.authentication.UsernamePasswordCredentials;
+import io.vertx.ext.web.RoutingContext;
+import java.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OptionalBasicAuthHandler implements Handler<RoutingContext> {
+  private static final Logger LOG = LoggerFactory.getLogger(OptionalBasicAuthHandler.class);
+
+  @Override
+  public void handle(RoutingContext routingContext) {
+    String authorization = routingContext.request().getHeader(HttpHeaders.AUTHORIZATION);
+    if (authorization != null) {
+      try {
+        String usernamePassword =
+            new String(Base64.getDecoder().decode(authorization.replace("Basic ", "")));
+        String[] split = usernamePassword.split(":", 2);
+        Credentials credentials;
+        if (split.length == 2) {
+          credentials = new UsernamePasswordCredentials(split[0], split[1]);
+        } else {
+          credentials = new UsernamePasswordCredentials(split[0], null);
+        }
+        // just put the credentials into the context user if available, no need for validity check
+        routingContext.setUser(User.create(credentials.toJson()));
+      } catch (RuntimeException e) {
+        LOG.warn("Unable to parse authorization header {}", authorization);
+      }
+    }
+    routingContext.next();
+  }
+}
